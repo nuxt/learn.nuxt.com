bb3f7e1cfc42a0b63d7a8a35cda931ccbb948534
l4dybird <future.l4dy61rd@gmail.com>
2023-11-23 22:53:28 +0900

    chore: Invert icon

--- components/ColorSchemeToggle.vue
+++ components/ColorSchemeToggle.vue
@@ -9,6 +9,6 @@ const toggleMode = () => colorMode.value = colorMode.value === 'light' ? 'dark' 
     class="p-2 rounded hover:bg-active"
     @click="toggleMode"
   >
-    <div class="i-carbon-moon dark:i-carbon-sun " />
+    <div class="i-carbon-sun dark:i-carbon-moon" />
   </button>
 </template>
