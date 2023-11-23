e21bed06ca18e7204dae1b42d0f549e427e65ea0
Rafał Piechowicz <piechowicz.rafal98@gmail.com>
2023-11-23 19:50:23 +0100

    feat: clean up and move webcontainer logic to composable

--- components/ThePlayground.client.vue
+++ components/ThePlayground.client.vue
@@ -3,89 +3,32 @@
 import { Pane, Splitpanes } from 'splitpanes'
 
 const iframe = ref<HTMLIFrameElement>()
-const wcUrl = ref<string>()
 
-type Status = 'init' | 'mount' | 'install' | 'start' | 'ready' | 'error'
-
-const status = ref<Status>('init')
-const error = shallowRef<{ message: string }>()
 const isDragging = usePanelDragging()
 
 const panelSizeEditor = usePanelCookie('nuxt-playground-panel-editor', 30)
 const panelSizeFrame = usePanelCookie('nuxt-playground-panel-frame', 30)
 
-const stream = ref<ReadableStream>()
-
-async function startDevServer() {
-  const tree = globFilesToWebContainerFs(
-    '../templates/basic/',
-    import.meta.glob([
-      '../templates/basic/**/*.*',
-      '!**/node_modules/**',
-    ], {
-      as: 'raw',
-      eager: true,
-    }),
-  )
-
-  console.log({ tree })
-
-  const wc = await useWebContainer()
-
-  wc.on('server-ready', (port, url) => {
-    status.value = 'ready'
-    wcUrl.value = url
-  })
-
-  wc.on('error', (err) => {
-    status.value = 'error'
-    error.value = err
-  })
-
-  status.value = 'mount'
-  await wc.mount(tree)
-
-  status.value = 'install'
-
-  const installProcess = await wc.spawn('pnpm', ['install'])
-  stream.value = installProcess.output
-  const installExitCode = await installProcess.exit
-
-  if (installExitCode !== 0) {
-    status.value = 'error'
-    error.value = {
-      message: `Unable to run npm install, exit as ${installExitCode}`,
-    }
-    throw new Error('Unable to run npm install')
-  }
-
-  status.value = 'start'
-  const devProcess = await wc.spawn('pnpm', ['run', 'dev'])
-  stream.value = devProcess.output
-
-  // In dev, when doing HMR, we kill the previous process while reusing the same WebContainer
-  if (import.meta.hot) {
-    import.meta.hot.accept(() => {
-      devProcess.kill()
-    })
-  }
-}
+const {
+  webcontainerUrl,
+  webcontainerStatus,
+  stream,
+  loadWebContainer,
+} = await useWebContainer()
 
 function startDragging() {
   isDragging.value = true
 }
+
 function endDragging(e: { size: number }[]) {
   isDragging.value = false
   panelSizeEditor.value = e[0].size
   panelSizeFrame.value = e[1].size
 }
 
-watchEffect(() => {
-  if (iframe.value && wcUrl.value)
-    iframe.value.src = wcUrl.value
+onMounted(async () => {
+  await loadWebContainer()
 })
-
-onMounted(startDevServer)
 </script>
 
 <template>
@@ -98,14 +41,16 @@ onMounted(startDevServer)
     </Pane>
     <Pane :size="panelSizeFrame" min-size="10">
       <iframe
-        v-show="status === 'ready'" ref="iframe" w-full h-full
-        :class="{
-          'pointer-events-none': isDragging,
-        }"
+        v-if="!!webcontainerUrl"
+        ref="iframe"
+        :src="webcontainerUrl"
+        :class="{ 'pointer-events-none': isDragging }"
+        w-full h-full
+        allow="geolocation; microphone; camera; payment; autoplay; serial; cross-origin-isolated"
       />
-      <div v-if="status !== 'ready'" flex="~ col items-center justify-center" h-full capitalize text-lg>
+      <div v-if="webcontainerStatus !== 'ready'" flex="~ col items-center justify-center" h-full capitalize text-lg>
         <div i-svg-spinners-90-ring-with-bg />
-        {{ status }}ing...
+        {{ webcontainerStatus }}ing...
       </div>
     </Pane>
     <Pane>
--- composables/webContainer.ts
+++ composables/webContainer.ts
@@ -1,11 +1,79 @@
 import { type FileSystemTree, WebContainer } from '@webcontainer/api'
 
 let _webContainerPromise: Promise<WebContainer>
+type Status = 'init' | 'mount' | 'install' | 'start' | 'ready' | 'error'
 
 export async function useWebContainer() {
   if (!_webContainerPromise)
     _webContainerPromise = WebContainer.boot()
-  return await _webContainerPromise
+
+  const webcontainerStatus = ref<Status>('init')
+  const webcontainerUrl = ref<string>()
+  const stream = ref<ReadableStream>()
+
+  const webcontainerInstance = await _webContainerPromise
+
+  const tree = globFilesToWebContainerFs(
+    '../templates/basic/',
+    import.meta.glob([
+      '../templates/basic/**/*.*',
+      '!**/node_modules/**',
+    ], {
+      as: 'raw',
+      eager: true,
+    }),
+  )
+
+  function setWebContainerStatus(value: Status): void {
+    webcontainerStatus.value = value
+  }
+
+  async function installDependencies() {
+    const installProcess = await webcontainerInstance.spawn('pnpm', ['install'])
+    return installProcess.exit
+  }
+
+  async function startDevServer() {
+    setWebContainerStatus('start')
+    const devProcess = await webcontainerInstance.spawn('pnpm', ['run', 'dev'])
+
+    stream.value = devProcess.output
+
+    webcontainerInstance.on('server-ready', (_, url) => {
+      if (url)
+        webcontainerUrl.value = url
+
+      setWebContainerStatus('ready')
+    })
+
+    webcontainerInstance.on('error', () => {
+      setWebContainerStatus('error')
+    })
+
+    if (import.meta.hot) {
+      import.meta.hot.accept(() => {
+        devProcess.kill()
+      })
+    }
+  }
+
+  async function loadWebContainer() {
+    setWebContainerStatus('mount')
+    await webcontainerInstance.mount(tree)
+
+    setWebContainerStatus('install')
+    const exitCode = await installDependencies()
+    if (exitCode !== 0)
+      throw new Error('Installation failed')
+
+    await startDevServer()
+  }
+  return {
+    stream,
+    webcontainerUrl,
+    webcontainerStatus,
+    loadWebContainer,
+  }
 }
 
 export function globFilesToWebContainerFs(
