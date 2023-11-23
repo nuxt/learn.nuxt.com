71288d343c6ad4e552b27137f9f745961f3aa751
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-23 22:35:49 +0100

    chore: revert some changes

--- components/ThePlayground.client.vue
+++ components/ThePlayground.client.vue
@@ -3,31 +3,95 @@
 import { Pane, Splitpanes } from 'splitpanes'
 
 const iframe = ref<HTMLIFrameElement>()
+const wcUrl = ref<string>()
+
+type Status = 'init' | 'mount' | 'install' | 'start' | 'ready' | 'error'
+
+const status = ref<Status>('init')
+const error = shallowRef<{ message: string }>()
 
 const isDragging = usePanelDragging()
 const panelSizeEditor = usePanelCookie('nuxt-playground-panel-editor', 30)
 const panelSizeFrame = usePanelCookie('nuxt-playground-panel-frame', 30)
 
-const {
-  webcontainerUrl,
-  webcontainerStatus,
-  stream,
-  loadWebContainer,
-} = await useWebContainer()
+const stream = ref<ReadableStream>()
+
+async function startDevServer() {
+  const tree = globFilesToWebContainerFs(
+    '../templates/basic/',
+    import.meta.glob([
+      '../templates/basic/**/*.*',
+      '../templates/basic/**/.*',
+      '!../.DS_Store',
+      '!**/node_modules/**',
+    ], {
+      as: 'raw',
+      eager: true,
+    }),
+  )
+
+  console.log({ tree })
+
+  const wc = await useWebContainer()
+
+  wc.on('server-ready', (port, url) => {
+    // Nuxt listen to multiple ports, and 'server-ready' is emitted for each of them
+    // We need the main one
+    if (port === 3000) {
+      status.value = 'ready'
+      wcUrl.value = url
+    }
+  })
+
+  wc.on('error', (err) => {
+    status.value = 'error'
+    error.value = err
+  })
+
+  status.value = 'mount'
+  await wc.mount(tree)
+
+  status.value = 'install'
+
+  const installProcess = await wc.spawn('pnpm', ['install'])
+  stream.value = installProcess.output
+  const installExitCode = await installProcess.exit
+
+  if (installExitCode !== 0) {
+    status.value = 'error'
+    error.value = {
+      message: `Unable to run npm install, exit as ${installExitCode}`,
+    }
+    throw new Error('Unable to run npm install')
+  }
+
+  status.value = 'start'
+  const devProcess = await wc.spawn('pnpm', ['run', 'dev'])
+  stream.value = devProcess.output
+
+  // In dev, when doing HMR, we kill the previous process while reusing the same WebContainer
+  if (import.meta.hot) {
+    import.meta.hot.accept(() => {
+      devProcess.kill()
+    })
+  }
+}
 
 function startDragging() {
   isDragging.value = true
 }
-
 function endDragging(e: { size: number }[]) {
   isDragging.value = false
   panelSizeEditor.value = e[0].size
   panelSizeFrame.value = e[1].size
 }
 
-onMounted(async () => {
-  await loadWebContainer()
+watchEffect(() => {
+  if (iframe.value && wcUrl.value)
+    iframe.value.src = wcUrl.value
 })
+
+onMounted(startDevServer)
 </script>
 
 <template>
@@ -36,28 +100,22 @@ onMounted(async () => {
     @resize="startDragging" @resized="endDragging"
   >
     <Pane :size="panelSizeEditor" min-size="10">
-      <PanelEditor />
+      [This is the editor]
     </Pane>
     <Pane :size="panelSizeFrame" min-size="10">
-      <div flex="~ gap-2 items-center" px4 py2 border="b base dashed" bg-faded>
-        <div i-ph-globe-duotone />
-        <span text-sm>Preview</span>
-      </div>
       <iframe
-        v-if="!!webcontainerUrl"
-        ref="iframe"
-        :src="webcontainerUrl"
-        :class="{ 'pointer-events-none': isDragging }"
-        w-full h-full
-        allow="geolocation; microphone; camera; payment; autoplay; serial; cross-origin-isolated"
+        v-show="status === 'ready'" ref="iframe" w-full h-full
+        :class="{
+          'pointer-events-none': isDragging,
+        }"
       />
-      <div v-if="webcontainerStatus !== 'ready'" flex="~ col items-center justify-center" h-full capitalize text-lg>
+      <div v-if="status !== 'ready'" flex="~ col items-center justify-center" h-full capitalize text-lg>
         <div i-svg-spinners-90-ring-with-bg />
-        {{ webcontainerStatus }}ing...
+        {{ status }}ing...
       </div>
     </Pane>
     <Pane>
-      <PanelTerminal :stream="stream" />
+      <TerminalOutput :stream="stream" />
     </Pane>
   </Splitpanes>
 </template>
