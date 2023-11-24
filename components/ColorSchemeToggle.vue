cf0e0cdd0874b364563886d5727f0c327a2f6823
_lmmmmmm <lmmmmmm12138@gmail.com>
2023-11-24 14:36:18 +0800

    style: transition api support

--- components/ColorSchemeToggle.vue
+++ components/ColorSchemeToggle.vue
@@ -1,7 +1,45 @@
 <script lang="ts" setup>
 const colorMode = useColorMode()
 
-const toggleMode = () => colorMode.value = colorMode.value === 'light' ? 'dark' : 'light'
+function toggleMode(event: MouseEvent) {
+  const nextColorMode = colorMode.value === 'light' ? 'dark' : 'light'
+
+  // @ts-expect-error experimental API missing type
+  const isAppearanceTransition = document.startViewTransition
+    && !window.matchMedia('(prefers-reduced-motion: reduce)').matches
+
+  if (!isAppearanceTransition) {
+    colorMode.value = nextColorMode
+    return
+  }
+
+  const [x, y] = [event.clientX, event.clientY]
+  const endRadius = Math.hypot(
+    Math.max(x, innerWidth - x),
+    Math.max(y, innerHeight - y),
+  )
+
+  // @ts-expect-error: Transition API missing type
+  const transition = document.startViewTransition(async () => {
+    colorMode.value = nextColorMode
+    await nextTick()
+  })
+  transition.ready
+    .then(() => {
+      const clipPath = [
+        `circle(0px at ${x}px ${y}px)`,
+        `circle(${endRadius}px at ${x}px ${y}px)`,
+      ]
+      document.documentElement.animate(
+        { clipPath },
+        {
+          duration: 400,
+          easing: 'ease-out',
+          pseudoElement: '::view-transition-new(root)',
+        },
+      )
+    })
+}
 </script>
 
 <template>
