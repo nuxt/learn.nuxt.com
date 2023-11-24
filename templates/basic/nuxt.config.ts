4c8afb15a21adf402ac829cace885977c9011595
Rafał Piechowicz <piechowicz.rafal98@gmail.com>
2023-11-24 13:59:42 +0100

    fix: remove unocss and color mode modules

--- templates/basic/nuxt.config.ts
+++ templates/basic/nuxt.config.ts
@@ -1,10 +1,3 @@
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  modules: [
-    '@nuxtjs/color-mode',
-    '@unocss/nuxt',
-  ],
-  colorMode: {
-    classSuffix: '',
-  },
 })
