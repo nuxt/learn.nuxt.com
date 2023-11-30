b2f2662f9b993a696d6252809253979956de7c89
Anthony Fu <anthonyfu117@hotmail.com>
2023-11-30 17:26:23 +0100

    feat: try to speed up container start up time

--- templates/basic/.layer-playground/nuxt.config.ts
+++ templates/basic/.layer-playground/nuxt.config.ts
@@ -2,6 +2,18 @@ import { defineNuxtConfig } from 'nuxt/config'
 
 export default defineNuxtConfig({
   devtools: { enabled: false },
+  ssr: false,
+  vite: {
+    warmupEntry: false,
+    optimizeDeps: {
+      include: [
+        'birpc',
+        'ufo',
+        'ofetch',
+        'defu',
+      ],
+    },
+  },
   typescript: {
     includeWorkspace: true,
     tsConfig: {
