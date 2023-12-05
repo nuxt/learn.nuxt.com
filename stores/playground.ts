393debec54124785d03719831a66cbeed59ea502
Cheah Shao Qi <52320846+sqcheah@users.noreply.github.com>
2023-12-05 23:47:39 +0800

    add back updatePreviewUrl

--- stores/playground.ts
+++ stores/playground.ts
@@ -65,6 +65,7 @@ export const usePlaygroundStore = defineStore('playground', () => {
             origin: url,
             fullPath: '/',
           }
+          updatePreviewUrl()
         }
       })
 
