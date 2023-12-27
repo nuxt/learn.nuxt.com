4f974506729c622bd756567c6c2ef77b4e12c8ec
Anthony Fu <anthonyfu117@hotmail.com>
2023-12-27 15:50:40 +0100

    chore: update

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -2,18 +2,26 @@
 const router = useRouter()
 const play = usePlaygroundStore()
 
-const metaMap = import.meta.glob('~/content/views/*/.patch/index.ts')
+const templatesMap = Object.fromEntries(
+  Object.entries(import.meta.glob('~/content/**/.template/index.ts'))
+    .map(([key, loader]) => [
+      key
+        .replace(/^\/content/, '')
+        .replace(/\/\.template\/index\.ts$/, '')
+        .replace(/\/\d+\./g, '/'),
+      loader,
+    ]),
+)
+
+if (process.dev)
+  // eslint-disable-next-line no-console
+  console.log('templates', Object.keys(templatesMap))
 
 async function mount(path: string) {
-  if (path.startsWith('/views/')) {
-    for (const [key, loader] of Object.entries(metaMap)) {
-      if (key.includes(path.replace(/^\/views\//, '')))
-        play.mountGuide(await loader().then((m: any) => m.meta))
-    }
-  }
-  else {
+  if (templatesMap[path])
+    play.mountGuide(await templatesMap[path]().then((m: any) => m.meta))
+  else
     play.mountGuide() // unmount previous guide
-  }
 }
 
 router.afterEach(async (to) => {
