e23c061f38801180eaf2af8846f105b7d87af2eb
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-16 10:50:53 +0100

    feat: use the same og image as nuxt.com

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
   },
   ogImage: {
     defaults: {
-      component: 'NuxtSeo',
+      component: 'OgImageDocs',
       props: {
         colorMode: 'dark',
       },
