0282674ff35a943183275798703972dc1c5892bf
Anthony Fu <anthonyfu117@hotmail.com>
2024-01-17 11:12:34 +0100

    fix: update cookie default value

--- stores/ui.ts
+++ stores/ui.ts
@@ -19,9 +19,18 @@ export const useUiState = defineStore('ui', () => {
 
   const stateCookie = useCookie<Partial<typeof persistState>>(
     'nuxt-playground-ui-state',
-    { default: () => ({}), watch: true },
+    { default: () => getLayoutDefaults(), watch: true },
   )
 
+  // assign default value if missing keys on migraton
+  const defaults = getLayoutDefaults()
+  const keys = Object.keys(defaults) as (keyof typeof defaults)[]
+  keys.forEach((key) => {
+    if (stateCookie.value[key] == null)
+      stateCookie.value[key] = defaults[key] as any
+  })
+
+  // update and sync cookie with the reactive state
   Object.assign(persistState, stateCookie.value)
   watch(persistState, () => {
     stateCookie.value = { ...persistState }
