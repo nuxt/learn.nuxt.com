4e48d0517ee83414d2b3d5a2d68c61d1b35950b2
Alex Liu <dsa1314@gmail.com>
2025-03-01 17:04:17 +0800

    update examples and solutions

---
+++ content/2.concepts/4.auto-imports/.template/files/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const count = ref(0)
+</script>
+
+<template>
+  <button type="button" @click="count++">
+    count is: {{ count }}
+  </button>
+
+  <Counter />
+</template>
