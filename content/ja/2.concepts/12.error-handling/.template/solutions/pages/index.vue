826f3cc47734149f111eae726ca438b13f7d7adf
Hikaru Kobayashi <hikaru.kobayashi0719@gmail.com>
2025-10-19 16:46:26 +0900

    fix: add files to solutions

---
+++ content/ja/2.concepts/12.error-handling/.template/solutions/pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { data: todos } = useFetch('/api/todos')
+</script>
+
+<template>
+  <ul>
+    <li v-for="todo in todos" :key="todo.id">
+      <NuxtLink :to="`/todo/${todo.id}`">
+        {{ todo.title }}
+      </NuxtLink>
+    </li>
+  </ul>
+</template>
