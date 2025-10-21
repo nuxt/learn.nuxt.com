0b1c7f6d14f8cac66de03558a5f7cb684dc5ee06
Urawa <uk.sigtan.uk@gmail.com>
2025-10-21 22:00:05 +0900

    feat: add lifecycle content
    ライフサイクルフックに関するコンテンツを追加

---
+++ content/ja/01.vue/04.lifecycle/.template/files/app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Child from './Child.vue'
+</script>
+
+<template>
+  <div>
+    <button>子コンポーネントを切り替え</button>
+    <p>マウント回数: </p>
+    <p>アンマウント回数: </p>
+  </div>
+</template>
